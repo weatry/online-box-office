1d82a7259aadebb21718bc5083199cdde9ee4ff4
weatry <weatry@gmail.com>
2022-07-30 20:14:24 +0800

    interact with other service by RestTemplate

---
+++ obo-schedule/src/main/java/com/github/budwing/obo/schedule/service/impl/DefaultScheduleService.java
@@ -0,0 +1,70 @@
+package com.github.budwing.obo.schedule.service.impl;
+
+import com.github.budwing.obo.schedule.entity.Schedule;
+import com.github.budwing.obo.schedule.entity.Ticket;
+import com.github.budwing.obo.schedule.repository.ScheduleRepository;
+import com.github.budwing.obo.schedule.repository.TicketRepository;
+import com.github.budwing.obo.schedule.service.ScheduleService;
+import com.github.budwing.obo.schedule.vo.Seat;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class DefaultScheduleService implements ScheduleService {
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    @Transactional
+    public Schedule changeStatus(String scheduleId, Schedule.Status status) {
+        Optional<Schedule> optional = scheduleRepository.findById(scheduleId);
+        if (!optional.isPresent()) {
+            log.debug("schedule is null");
+            return null;
+        }
+
+        Schedule schedule = optional.get();
+        Schedule.Status preStatus = schedule.getStatus();
+        if(preStatus.ordinal()>=status.ordinal()) {
+            log.info("schedule status is {}, which can be changed to {}", preStatus, status);
+            return schedule;
+        }
+
+        switch (status) {
+            case SALE:
+                List<Ticket> tickets = ticketsOf(schedule);
+                ticketRepository.saveAll(tickets);
+                break;
+            case CANCEL:
+                ticketRepository.deleteBySchedule_id(scheduleId);
+        }
+
+        schedule.setStatus(status);
+        scheduleRepository.save(schedule);
+
+        return schedule;
+    }
+
+    public List<Ticket> ticketsOf(Schedule schedule) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format("http://localhost:8081/obo/cinema/%d/hall/%d/seat",
+                schedule.getCinemaId(), schedule.getHallId());
+        log.debug("send request to {} to query seats", url);
+        List<Map> seats = restTemplate.getForObject(url, List.class);
+        log.debug("got seats:{}", seats);
+        return seats.stream()
+                .map(m -> new Ticket(schedule, Seat.of(m), 40.00))
+                .collect(Collectors.toList());
+    }
+}
