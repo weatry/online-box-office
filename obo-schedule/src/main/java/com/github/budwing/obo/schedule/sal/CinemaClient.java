fa4d569fa2d733343c6dc7b2682cad256466e55c
weatry <weatry@gmail.com>
2022-08-23 16:26:27 +0800

    enable Feign internal Circuit Breaker support

--- obo-schedule/src/main/java/com/github/budwing/obo/schedule/sal/CinemaClient.java
+++ obo-schedule/src/main/java/com/github/budwing/obo/schedule/sal/CinemaClient.java
@@ -1,16 +1,32 @@
 package com.github.budwing.obo.schedule.sal;
 
 import com.github.budwing.obo.schedule.vo.Seat;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient("obo-cinema")
+@FeignClient(value = "obo-cinema", fallbackFactory = CinemaClientFallbackFactory.class)
 public interface CinemaClient {
     @GetMapping("/obo/cinema/{cinemaId}/hall/{hallId}/seat")
-    @CircuitBreaker(name = "obo-cinema")
     List<Seat> getSeatOf(@PathVariable Integer cinemaId, @PathVariable Integer hallId);
+}
+
+@Component
+@Slf4j
+class CinemaClientFallbackFactory implements FallbackFactory<CinemaClient> {
+    @Override
+    public CinemaClient create(Throwable cause) {
+        log.error("fallback for exception: {}", cause.getMessage());
+        return new CinemaClient() {
+            @Override
+            public List<Seat> getSeatOf(Integer cinemaId, Integer hallId) {
+                return null;
+            }
+        };
+    }
 }
